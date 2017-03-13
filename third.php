6c30d7cc0e8e04e60e617aa7d62c443521489b5b
viliusnorbutas <norbutasvilius@gmail.com>
2017-03-13 12:28:34 +0200

    added third.php

---
+++ third.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: USER
+ * Date: 3/13/2017
+ * Time: 12:28 PM
+ */
+
+class homework {
+
+    public $text = "This is third.php - git-homework.";
+
+    public function homeworkText() {
+        return $this->text;
+    }
+}
+
+$homework = new homework();
+
+echo $homework->homeworkText();
