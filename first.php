27188b4b6905dee67b36b2c2551c048fb1aa7304
viliusnorbutas <norbutasvilius@gmail.com>
2017-03-13 12:26:46 +0200

    added first.php

---
+++ first.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: USER
+ * Date: 3/13/2017
+ * Time: 12:26 PM
+ */
+
+class homework {
+
+    public $text = "This is first.php - git-homework.";
+
+    public function homeworkText() {
+        return $this->text;
+    }
+}
+
+$homework = new homework();
+
+echo $homework->homeworkText();
